06b88d0eaa035387c099ee05d1ac2a06f2a28ef9
Nitansh <nitansh11@gmail.com>
2020-12-31 13:25:35 +0530

    dummy data

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-localStorage.setItem("currentUser",JSON.stringify({name:"Nitansh Rastogi",email:"nitansh11@gmail.com"}));
+//This is just a dummy data ,ignore this
+let obj = {name:"Nitansh Rastogi",email:"nitansh11@gmail.com",waterLog:[
+    {date: "1-12-2020", noOfGlasses: 3},
+    {date: "2-12-2020", noOfGlasses: 4},
+    {date: "3-12-2020", noOfGlasses: 5},
+    {date: "4-12-2020", noOfGlasses: 8},
+    {date: "5-12-2020", noOfGlasses: 13},
+    {date: "6-12-2020", noOfGlasses: 17},
+    {date: "7-12-2020", noOfGlasses: 4},
+    {date: "8-12-2020", noOfGlasses: 12},
+    {date: "9-12-2020", noOfGlasses: 7},
+    {date: "10-12-2020", noOfGlasses: 4},
+    {date: "11-12-2020", noOfGlasses: 7},
+    {date: "12-12-2020", noOfGlasses: 9},
+    {date: "13-12-2020", noOfGlasses: 11},
+    {date: "14-12-2020", noOfGlasses: 15},
+    {date: "15-12-2020", noOfGlasses: 19},
+    {date: "16-12-2020", noOfGlasses: 20},
+    {date: "17-12-2020", noOfGlasses: 5},
+    {date: "18-12-2020", noOfGlasses: 2},
+    {date: "19-12-2020", noOfGlasses: 6},
+    {date: "20-12-2020", noOfGlasses: 6},
+    {date: "21-12-2020", noOfGlasses: 9},
+    {date: "22-12-2020", noOfGlasses: 2},
+    {date: "23-12-2020", noOfGlasses: 15},
+    {date: "24-12-2020", noOfGlasses: 18},
+    {date: "25-12-2020", noOfGlasses: 19},
+    {date: "26-12-2020", noOfGlasses: 12},
+    {date: "27-12-2020", noOfGlasses: 6},
+    {date: "28-12-2020", noOfGlasses: 16},
+    {date: "29-12-2020", noOfGlasses: 2},
+    {date: "30-12-2020", noOfGlasses: 7},
+    {date: "31-12-2020", noOfGlasses: 11}
+]};
+localStorage.setItem("currentUser",JSON.stringify(obj));
